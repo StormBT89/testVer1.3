098775c977c85d1199a8508182c271d5f9ebf148
StormBT89 <glavevski.pece@gmail.com>
2024-11-02 05:10:55 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Форма за внесување - тестирање Ангела драфт верзија 1.3</h1>
+    </header>
+  );
+}
+
+export default Header;
